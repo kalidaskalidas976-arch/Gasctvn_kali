0fd8cdab1105f1afe9547e2314a9b5ca71b64afb
kalidaskalidas976-arch <kalidaskalidas976@gmail.com>
2025-09-02 10:10:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
-const search = document.querySelector('.search')
-const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+const body = document.body
 
-btn.addEventListener('click', () => {
-    search.classList.toggle('active')
-    input.focus()
-})
+body.addEventListener('dragstart', dragStart)
+body.addEventListener('dragend', dragEnd)
+
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+}
+
+function dragStart(e) {
+    if(!e.target.classList.contains("fill")) {
+        e.preventDefault()
+        return
+    }
+    fill.className += ' hold' 
+    setTimeout(() => fill.className = 'invisible', 0)
+}
+
+function dragEnd() {
+    fill.className = 'fill'
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
+}
+
+function dragLeave() {
+    this.className = 'empty'
+}
+
+function dragDrop() {
+    this.className = 'empty'
+    this.append(fill)
+}
